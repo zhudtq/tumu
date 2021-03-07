71801b07d8345e1fbb994df38c384ef0392a4c82
QuanZhu <zhudtqzz@gmail.com>
2021-03-07 23:58:36 +0800

    feat: 首页

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+          标题
+      </div>
+      <div className="body">
+        <button onClick={clickMe}>
+          点击我
+        </button>
+      </div>
     </div>
   );
+}
+
+function clickMe() {
+  console.log('nicky 我被点了')
 }
 
 export default App;
